1bc484c410f8ff60372315b57d37716c47751639
afrotechnologist <h.meharitesfay@gmail.com>
2024-02-03 18:53:50 +0100

    test commit

--- projekt.cs
+++ projekt.cs
@@ -2,3 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 
 [Route("api/[controller]")]
+
+public class RovarspraketController : ControllerBase
+{
+    [HttpPost("encrypt")]
+
+    public ActionResult<string> Encrypt([FromBody] string text)
+
+    {
+        // Krypteringslogik med rövarspråket
+        // Implementera krypteringsalgoritmen här
+        throw new NotImplementedException();
+    }
+    
+}
